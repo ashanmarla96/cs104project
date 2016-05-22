95b4abdac00e5b87aa4cd9330c1ec53911a2f5e5
Ashan Marla <amarla@>
2016-05-21 17:43:31 -0700

    sumpairs mostly working

--- hw1/sum_pairs.cpp
+++ hw1/sum_pairs.cpp
@@ -1,12 +1,63 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <cmath>
 using namespace std;
 
-
-
-
-
 int main(int argc, char* argv[]){
+	if(argc < 3){
+		cerr << "Include an input and output file" << endl;
+		return 1;
+	}
+
+	ifstream infile(argv[1]);
+	ofstream outfile(argv[2]);
+	int count = 0;
+	int num = 0;
+	
+	if(infile.fail()){
+		cerr << "Error opening file";
+		return 1;
+	}
+
+	infile >> count;
+	int* numList = new int[count];
+
+	for(int i=0; i<count; i++){
+		infile >> num;
+		numList[i] = num;
+	}
+
+	infile.close();
+	int newcap = ceil(count/2);
+	int* sumList = new int[newcap];
+
+	for(int i=0; i<count; i++){
+		for (int j = count-1; j >= 0; j--)
+		{
+			sumList[i] = numList[i] + numList[j];
+			if(i+1 == j || i == j){
+				break;
+			}
+		}
+	}
+
+	if (outfile.fail()){
+		cerr << "Error opening output file";
+	}
+	else{
+		if(count == 0){
+			outfile << 0;
+		}
+		else{
+			for(int i=0; i < newcap; i++){
+				outfile << sumList[i];
+			}
+		}
+	}
+
+	delete [] numList;
+	delete [] sumList;
 
 }
 

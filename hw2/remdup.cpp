b534cbb1faec22c4f06d3a95bf5f0c2c15a62629
Ashan Marla <amarla@>
2016-06-03 23:59:18 -0700

    updated hw2.txt

--- hw2/remdup.cpp
+++ hw2/remdup.cpp
@@ -2,12 +2,26 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct Item{
 	int val;
 	Item* next;
 };
+
+void print(Item*& head){
+	//ofstream outfile(ofile);
+	//vector<int> nums;
+	if(head->next == NULL){
+		return;
+	}  
+	else{
+		cout << head->val << endl;
+		//nums.push_back(head->val);
+   		print(head->next);
+  	}
+}
 
 void readLists(char* filename, Item*& headA, Item*& headB){
 	ifstream infile(filename);
@@ -16,18 +30,17 @@ void readLists(char* filename, Item*& headA, Item*& headB){
 		return;
 	}
 
-	
-	
 	string txtline;
 	int num;
 	getline(infile, txtline);
 	stringstream ss(txtline);
-	cout<<txtline<<endl;
+	//cout<<txtline<<endl;
 	Item* temp;
 	while(ss >> num){
 		Item* newNode = new Item;
 		newNode->val = num;
 		newNode->next = NULL;
+
 		if(headA == NULL){
 			headA = newNode;
 			temp = headA;
@@ -35,38 +48,55 @@ void readLists(char* filename, Item*& headA, Item*& headB){
 		else{
 			temp->next = newNode;
 			temp = temp->next;
-			//temp = newNode;
 		}
-		
 	}
-	
-	/*Item* temp2 = headA;
-	while(temp2 != NULL){
-		cout << temp2->val << endl;
-		temp2 = temp2->next;
-	}*/
+
+	string txtline2;
+	getline(infile, txtline2);
+	stringstream ss2(txtline2);
+	while(ss2 >> num){
+		Item* newNode = new Item;
+		newNode->val = num;
+		newNode->next = NULL;
+
+		if(headB == NULL){
+			headB = newNode;
+			temp = headB;
+		}
+		else{
+			temp->next = newNode;
+			temp = temp->next;
+		}
+	}
+
 }
 
 
 
 void concatenatehelper(Item* headA, Item* headB, Item* headC){
-	if(headA == NULL){
-		Item* newNode = new Item;
-		headC->next = newNode;
-		newNode = headB;
-		concatenatehelper(headA, headB->next, headC);
-	}
+	//cout << "TEST0" << endl;
+	Item* newNode = new Item;
 	if(headB == NULL){
+		headC->next = NULL;
 		return;
 	}
+	else if(headA == NULL){
+		//Item* newNode = new Item;
+		newNode->val = headB->val;
+		headC->val = newNode->val;
+		concatenatehelper(headA, headB->next, headC->next);
+	}
 	else{
-		headC->next = headA;
-		concatenatehelper(headA->next, headB, headC);
+		//Item* newNode = new Item;
+		newNode->val = headA->val;
+		headC->val = newNode->val;
+		concatenatehelper(headA->next, headB, headC->next);
 	}
 }
 
 Item* concatenate(Item* headA, Item* headB){
 	Item* headC = new Item;
+	//headC->val = 0; headC->next = NULL;
 	concatenatehelper(headA, headB, headC);
 	return headC;
 }
@@ -95,21 +125,30 @@ void removeDuplicates(Item*& head){
 
 
 int main(int argc, char* argv[]){
-	if(argc < 2){
-   		cerr << "Usage ./remdup input_file output_file" << endl;
+	if(argc < 3){
+   		cerr << "Usage $(BIN_DIR)/remdup input_file output_file" << endl;
   	}
 
   	Item* head1 = NULL;
   	Item* head2 = NULL;
-  	Item* head3;
+  	Item* head3 = NULL;
 
   	readLists(argv[1], head1, head2);
 
+  	//vector<int> numlist;
   	removeDuplicates(head1);
-  	//head3 = concatenate(head1, head2);
-  	Item* temp2 = head1;
-	while(temp2 != NULL){
-		cout << temp2->val << endl;
-		temp2 = temp2->next;
-	}
+  	head3 = concatenate(head1, head2);
+  	print(head3);
+
+  	//ofstream outfile(argv[2]);
+
+  	Item* temp = head3;
+  	while(temp->next != NULL){
+  		cout << temp->val << " ";
+  		temp = temp->next;
+  	}
+  	cout << endl;
+
+  	
+
 }
